8236345e415d5d199cbf247b20d8c0f5921e030b
Chinwuba Israel <chiizzy5@gmail.com>
2022-07-28 08:20:45 +0100

    site update

--- js/configProjectTagColor.js
+++ js/configProjectTagColor.js
@@ -1,9 +1,10 @@
 (function () {
     // This object will color project tags based on their text content. For example, all HTML tags will be colored red.
     const TEXT_TO_COLOR = {
-        HTML: "#ff8f8f",
+        HTML: "#fff3f3",
         CSS: "#51eaff",
         JavaScript: "#ffe86f",
+        React: "skyblue",
         "REST APIs": "#51FFA2"
     }
 
